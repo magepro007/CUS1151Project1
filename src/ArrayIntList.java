0c642aa3e64fddff517f32e99a2138d58e6ec286
kurt1s <kurtis.bassmann18@stjohns.edu>
2018-09-26 18:16:03 -0400

    Added JavaDocs to ArrayIntList, including methods from Questions 1 to 4.

--- src/ArrayIntList.java
+++ src/ArrayIntList.java
@@ -5,35 +5,56 @@ import java.util.Stack;
            * Professor. Keshtkar
            * @author Kurtis Bassmann
            *    @TODO Figure out best way to implement method demonstrations for Q's 5-9. 
-           *    @TODO Write JavaDoc for all methods ***  
+           *    @TODO Sort the list after each insertion. Check documentation if this should be implemented.
            */
 public class ArrayIntList {
 	private int[] elementData;
 	private int size;
 	private final int DEFAULT_CAPACITY = 20;
 
+        /**
+         * Default Constructor: Allocates space for int array, elementData, for 20 positions in memory.
+         * Size is set to 0, since no elements have been added to the list.
+         */
 	public ArrayIntList() {
 		size = 0;
 		elementData = new int[DEFAULT_CAPACITY];
 	}
-
+        
+        /**
+         * Parameterized Constructor: Allocates the specified amount of memory positions for the array, elementData.
+         * Size is set to 0, since no elements have been added to the list.
+         * @param capacity the amount of memory to allocate for the ArrayIntList's inner array
+         */
 	public ArrayIntList(int capacity) {
 		size = 0;
 		elementData = new int[capacity];
 	}
 
+        /**
+         * Adds the integer argument, value, to the end of the list.
+         * If the list is full, an error message is printed in the console.
+         * @param value the integer value to add to the end of the list
+         */
 	public void add(int value) {
 		if(size == elementData.length)
-			System.out.println("The list is full, cannot add value.");
+			System.err.println("The list is full, cannot add value.");
 		else {
 			elementData[size] = value;
 			size++;
 		}
 	}
 
+        /**
+         * Adds the integer argument, value, to the index specified by integer argument, index.
+         * All elements in the array are shifted to their next respective position in the array, to allow space for the new value.
+         * If the list is full, an error message is printed in the console.
+         * @param index the position in elementData to add the new integer to.
+         * @param value the new value to add into the list
+         */
 	public void add(int index, int value) { // Check to see if List is full ?
 		if(size == elementData.length)
-			System.out.println("The list is full, cannot add value.");
+			System.err.println("The list is full, cannot add value.");
 		else {
 			for(int i = size - 1; i >= index; i--) {
 				elementData[i + 1] = elementData[i];
@@ -45,6 +66,11 @@ public class ArrayIntList {
 
 	}
 
+        /**
+         * Addresses whether or not the argument, value, is contained in the list.
+         * @param value the value being searched for in elementData
+         * @return returns true if the element is found, returns false otherwise
+         */
 	public boolean contains(int value) {
 		for(int i = 0; i < size; i++) {
 			if(elementData[i] == value)
@@ -53,21 +79,36 @@ public class ArrayIntList {
 		return false;
 	}
 
-	public void ensureCapacity(int capacity) { // will ensure that at a 'capacity-#' of elements can be stored
+        /**
+         * Ensures that the specified integer number of elements can be stored in the list's array.
+         * This is accomplished by creating a new array of this capacity, and then copying the elements from the current list, into the new list.
+         * The new list is then referenced by the instance variable, elementData.
+         * @param capacity the capacity of elements the list shall be modified to ensure
+         */
+	public void ensureCapacity(int capacity) {
 		int[] newAlloc = new int[capacity];
 		for(int i = 0; i < size; i++) {
 			newAlloc[i] = elementData[i];
 		}
 		this.elementData = newAlloc; 
-		//check This
 	}
 
+        /**
+         * Accesses an integer at the position specified by argument, index.
+         * @param index the position to access
+         * @return the integer value accessed
+         */
 	public int get(int index) {
 		if(index > size - 1)
 			return -1;
 		return elementData[index];
 	}
 
+        /**
+         * Finds the index, of the first occurrence, of the value specified.
+         * @param value the integer value to search for
+         * @return the specified value's position in the list
+         */
 	public int indexOf(int value) {
 		for(int i = 0; i < size; i++) {
 			if(elementData.equals(value))
@@ -76,10 +117,18 @@ public class ArrayIntList {
 		return -1;
 	}
 
+        /**
+         * Determines whether or not the list is empty.
+         * @return returns true if the list's size is 0, and false if size is non-zero.
+         */
 	public boolean isEmpty() {
 		return size == 0;
 	}
 
+        /**
+         * Removes the value at the specified index from the list, and shifts all elements after index to their respective indeces one less than their current.
+         * @param index the index of the value to remove from the list
+         */
 	public void remove(int index) {
 		for(int i = index; i < size - 1; i++) {
 			elementData[i] = elementData[i+1];
@@ -88,14 +137,30 @@ public class ArrayIntList {
 		//check this
 	}
 
-	public void set(int index, int value) { /*TODO: Check for bad index */
-		elementData[index] = value;
+        /**
+         * Sets the value at the index specified to the value specified.
+         * @param index the index to modify
+         * @param value the value to store at the specified index of the list
+         */
+	public void set(int index, int value) {
+            if(index < 0 || index > (size - 1))
+                System.err.println("The specified index is out of bounds");
+            elementData[index] = value;
 	}
 
+        /**
+         * Determines the size of the list
+         * @return the size of the elements stored in the list
+         */
 	public int size() {
 		return size;
 	}
 
+        /**
+         * Creates a String representation of all of the elements added to, and stored in the list.
+         * The size of the list is also included in this representation.
+         * @return a String containing the state of the list
+         */
 	public String toString() {
 		String str = "[";
 		for(int i = 0; i < this.size; i++) {
@@ -109,7 +174,12 @@ public class ArrayIntList {
 		//		return elementData.toString() + "\t(size: " + size + ")";
 	}
 
-	//Begin Project Questions...
+	// Questions 1 - 4 ...
+        
+        /**
+         * Determines at how many locations the most frequently occurring integer in the list has been stored.
+         * @return the number of occurrences of the most frequently occurring integer in the list
+         */
         public int maxCount() {
             int maxCount = 1; int count = 1;
             
@@ -125,6 +195,10 @@ public class ArrayIntList {
             
         }
         
+        /**
+         * Determines the length of the longest sorted sequence of integers stored in the list.
+         * @return the length of the longest sorted sequence
+         */
 	public int longestSortedSequence() {
 		int longest = 1;
 		for(int i = 0; i < this.size() - 1; i++) {
@@ -137,6 +211,11 @@ public class ArrayIntList {
 		return longest;
 	}
 
+        /**
+         * Creates a new list which stores the running total of the list at each index, i.
+         * The value at any position, i, in the new list is equal to the sum of the values in this list from positions 0 to i, inclusive.
+         * @return a new list which contains the running total of the list at each index.
+         */
         public ArrayIntList runningTotal() {
             int length = this.size;
             ArrayIntList sumList = new ArrayIntList(length);
@@ -150,6 +229,11 @@ public class ArrayIntList {
             return sumList;
         }
 
+        /**
+         * Determines if the list is pairwise sorted.
+         * A list is considered pairwise sorted if each successive pair of numbers is in sorted (non-decreasing) order.
+         * @return returns true if the list is pairwise sorted, or false otherwise
+         */
 	public boolean isPairWiseSorted() {
 		if(size == 1)
 			return true;
